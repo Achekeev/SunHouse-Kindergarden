fb1cc14c9d2faa29196aa9a745be98ab81cd6597
Ilyas <achekeev88@gmail.com>
2022-06-22 17:33:50 +0600

    Model added

--- main/models.py
+++ main/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Child(models.Model):
+    kids_first_name = models.CharField(max_length=255, verbose_name='Имя Ребенка')
+    fathers_name = models.CharField(max_length=255, verbose_name='Имя Отца')
+    mothers_name = models.CharField(max_length=255, verbose_name='Имя Матери')
+    birth_date = models.DateField()
+
